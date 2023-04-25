4776cb34d3bfbcd94312de6c74233a44a4480a01
liangjunming <allenprivate@163.com>
2023-04-25 14:28:35 +0800

    add package: flycheck

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 (require 'init-theme)
 (require 'init-lsp)
 (require 'init-lang-mode)
+(require 'init-flycheck)
 
 
 ;; 以下代码是emacs自动添加
@@ -45,7 +46,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dap-mode lsp-java company use-package))
+ '(package-selected-packages '(flycheck dap-mode lsp-java company use-package))
  '(warning-suppress-log-types '((use-package) (use-package)))
  '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
