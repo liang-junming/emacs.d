53ee33d3d81cf5fb98fe6a84ea506c68a6ed8ca1
liangjunming <allenprivate@163.com>
2023-04-28 19:36:16 +0800

    delete package: company-box
    对主题支持不好，弹框背景色和主题背景色一个颜色

--- config/init-company.el
+++ config/init-company.el
@@ -7,16 +7,16 @@
   (setq company-idle-delay 0)
   (setq company-echo-delay 0)
   (setq company-show-quick-access t)
-  (setq company-tooltip-limit 15)
+  (setq company-tooltip-limit 10)
   (setq company-minimum-prefix-length 1)
   (setq company-tooltip-align-annotations t)
   (setq company-tooltip-flip-when-above t)
   :config
   (global-company-mode))
 
-(use-package company-box
-  :ensure t
-  :hook (company-mode . company-box-mode))
+;;(use-package company-box
+;;  :ensure t
+;;  :hook (company-mode . company-box-mode))
 
 
 (provide 'init-company)
