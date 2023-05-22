6c0431f9a993565125ab7f79fac033a651b3abc2
liangjunming <allenprivate@163.com>
2023-05-22 11:23:52 +0800

    add package mwim to enhance C-a and C-e

--- config/init-tools.el
+++ config/init-tools.el
@@ -90,6 +90,14 @@
   (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
   (add-hook 'prog-mode-hook 'smartparens-mode))
 
+(use-package mwim
+  :ensure t
+  :config
+  (global-set-key (kbd "C-a") 'mwim-beginning-of-code-or-line)
+  (global-set-key (kbd "C-e") 'mwim-end-of-code-or-line)
+  (global-set-key (kbd "<home>") 'mwim-beginning-of-line-or-code)
+  (global-set-key (kbd "<end>") 'mwim-end-of-line-or-code))
+
 
 (provide 'init-tools)
 ;;; init-tools.el ends here
