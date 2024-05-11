900f306d4f3f7f14dd6346113c98cd7b03ced02f
梁军明 <liangjunming@liangjunmingdeiMac.local>
2024-05-11 17:31:19 +0800

    org-mode标题不通过颜色区分，通过字体大小区分

--- config/init-org.el
+++ config/init-org.el
@@ -20,6 +20,20 @@
 (setq org-agenda-files '("~/.org-agenda/"))
 (setq org-hide-emphasis-markers t)
 
+;; 设置org标题1-8级的字体大小和颜色
+(custom-set-faces
+  '(org-level-1 ((t (:inherit defualt :bold t :height 1.6))))
+  '(org-level-2 ((t (:inherit defualt :bold t :height 1.42))))
+  '(org-level-3 ((t (:inherit defualt :bold t :height 1.36))))
+  '(org-level-4 ((t (:inherit defualt :bold t :height 1.2))))
+  '(org-level-5 ((t (:inherit defualt :bold t :height 1.1))))
+  '(org-level-6 ((t (:inherit defualt :bold t :height 1.1))))
+  '(org-level-7 ((t (:inherit defualt :bold t :height 1.1))))
+  '(org-level-8 ((t (:inherit defualt :bold t :height 1.1))))
+ ) ;; end custom-set-faces
+
+(add-hook 'org-mode-hook 'org-indent-mode)
+
 
 (provide 'init-org)
 ;;; init-org.el ends here
